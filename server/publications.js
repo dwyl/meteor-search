c2ae42df833d3597ee955e9cf858ed82e3a93bdf
nelsonic <nodecoder@gmail.com>
2014-04-06 10:16:54 +0100

    moves publications to their own file for clarity

---
+++ server/publications.js
@@ -0,0 +1,22 @@
+Meteor.publish("posts", function () {
+  return Posts.find({}, {sort: {time: -1}, limit:25});
+});
+
+Meteor.publish("search_posts", function (post_ids) {
+  // console.log("PUBLISH", post_ids)
+  if(post_ids.length > 0){
+    return Posts.find({_id:{"$in":post_ids}},{sort: {time: -1}});
+  } else {
+    return Posts.find({}, {sort: {time: -1}, limit:25});
+  }
+  
+});
+
+Meteor.publish("search_results", function (keywords) {
+  console.log("s_r",keywords)
+  return Search_results.find({"keywords":keywords});
+});
+
+Meteor.publish("all_results", function (){
+  return Search_results.find({});
+});
